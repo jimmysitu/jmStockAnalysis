36e2aa5f3787a5aa37c127ca25df5e41b350d41d
Jimmy Situ <web@jimmystone.cn>
2025-02-15 11:32:38 +0800

    Add update database script and update tsla page

---
+++ updateData.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 -u
+
+import msfinance as msf
+
+proxy = 'socks5://127.0.0.1:1088'
+
+stock = msf.Stock(
+    debug=True,
+    database='sp500.db3',
+    proxy=proxy,
+)
+
+tickers_list = {
+    'tsla', # Tesla
+}
+
+for ticker in sorted(tickers_list):
+    key_metrics = stock.get_key_metrics(ticker, 'xnas', update=True)
+    financials = stock.get_financials(ticker, 'xnas', update=True)
+
+    print(f"Ticker: {ticker}")
+    for key_metric in key_metrics:
+        print(key_metric)
+    for financial in financials:
+        print(financial)
